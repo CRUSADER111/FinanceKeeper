4c71e21ddc52c7cea3b0a5ed91e7a063cc1bfd42
Alex Morrison <asmorrison@hotmail.co.uk>
2017-04-12 16:42:43 +0100

    DB Update

--- db/financedb.sql
+++ db/financedb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 11, 2017 at 05:29 PM
+-- Generation Time: Apr 12, 2017 at 05:41 PM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 5.6.28
 
@@ -42,6 +42,100 @@ CREATE TABLE `accounts` (
 INSERT INTO `accounts` (`AccountID`, `Password`, `Forename`, `Surname`, `Email`, `Income`) VALUES
 (4545, 'aaa', 'Alex', 'Morrison', 'asmorrison@hotmail.co.uk', '10000.0'),
 (4646, 'aaa', 'Alex', 'Test', 'test@gmail.com', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `expenses`
+--
+
+CREATE TABLE `expenses` (
+  `ExpenseID` int(11) NOT NULL,
+  `AccountID` int(11) NOT NULL,
+  `Expense` varchar(30) DEFAULT NULL,
+  `Value` varchar(30) DEFAULT NULL,
+  `DateAdded` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `utilities`
+--
+
+CREATE TABLE `utilities` (
+  `UtilityID` int(11) NOT NULL,
+  `AccountID` int(11) NOT NULL,
+  `Utility` varchar(30) DEFAULT NULL,
+  `Value` varchar(30) DEFAULT NULL,
+  `BillingCycle` varchar(30) DEFAULT NULL,
+  `DateAdded` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `utilities`
+--
+
+INSERT INTO `utilities` (`UtilityID`, `AccountID`, `Utility`, `Value`, `BillingCycle`, `DateAdded`) VALUES
+(1, 4545, 'Gas', '75.76', '3 Month', '12-Apr-2017'),
+(2, 4545, 'Water', '74.36', '3 Month', '12-Apr-2017'),
+(3, 4545, 'Water+', '1000', '4 Month', '12-Apr-2017'),
+(4, 4545, 'Water+', '1000', '4 Month', '12-Apr-2017'),
+(5, 4545, 'Water+', '1000', '4 Month', '12-Apr-2017');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`AccountID`);
+
+--
+-- Indexes for table `expenses`
+--
+ALTER TABLE `expenses`
+  ADD PRIMARY KEY (`ExpenseID`),
+  ADD KEY `AccountID` (`AccountID`);
+
+--
+-- Indexes for table `utilities`
+--
+ALTER TABLE `utilities`
+  ADD PRIMARY KEY (`UtilityID`),
+  ADD KEY `AccountID` (`AccountID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `expenses`
+--
+ALTER TABLE `expenses`
+  MODIFY `ExpenseID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `utilities`
+--
+ALTER TABLE `utilities`
+  MODIFY `UtilityID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `expenses`
+--
+ALTER TABLE `expenses`
+  ADD CONSTRAINT `FK_Expenses_Accounts` FOREIGN KEY (`AccountID`) REFERENCES `accounts` (`AccountID`);
+
+--
+-- Constraints for table `utilities`
+--
+ALTER TABLE `utilities`
+  ADD CONSTRAINT `FK_Utility_Accounts` FOREIGN KEY (`AccountID`) REFERENCES `accounts` (`AccountID`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
